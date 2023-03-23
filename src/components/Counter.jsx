7744d737876a2ae037130867c5c27c8705ba7b16
andyyablonskii <andyyablonskii@gmail.com>
2023-03-23 13:38:04 +0200

    k

---
+++ src/components/Counter.jsx
@@ -0,0 +1,26 @@
+import { isDisabled } from '@testing-library/user-event/dist/utils';
+import { useState } from 'react';
+import './Counter.css'
+
+const Counter = () => {
+
+  const [count, setCount] = useState(0);
+
+  function decrementCount() {
+    setCount(count - 1);
+  }
+
+  function incrementCount() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div className='counter' >
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
+    </div>
+  );
+};
+
+export default Counter;
